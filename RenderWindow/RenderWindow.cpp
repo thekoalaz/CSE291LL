80d6f6d1a336ee0e33827671b6ffdd918ab703ff
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2015-01-22 10:30:42 -0800

    Visual Studio solution setup.

---
+++ RenderWindow/RenderWindow.cpp
@@ -0,0 +1,14 @@
+// RenderWindow.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    std:: cout << "hi" << std::endl;
+
+	return 0;
+}
