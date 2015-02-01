82808be305df80153d03d74b4a744eeb1c1d3d8d
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2015-02-01 15:25:44 -0800

    Removed test code for reading HDR.

--- RenderWindow/RenderWindowMain.cpp
+++ RenderWindow/RenderWindowMain.cpp
@@ -38,26 +38,6 @@ int main(int argc, char* argv[])
     */
 
     Scene::EnvMap * envMap = new Scene::EnvMap();
-    std::tuple<float, float, float> color;
-    color = envMap->getColor(0.0, 0.0);
-    std::cout << "0.0, 0.0:" << std::endl;
-    std::cout << std::get<0>(color) << ", ";
-    std::cout << std::get<1>(color) << ", ";
-    std::cout << std::get<2>(color) << std::endl;
-    std::cout << "====================================================" << std::endl;
-    color = envMap->getColor(854.0, 424.0);
-    std::cout << "854, 424:" << std::endl;
-    std::cout << std::get<0>(color) << ", ";
-    std::cout << std::get<1>(color) << ", ";
-    std::cout << std::get<2>(color) << std::endl;
-    std::cout << "====================================================" << std::endl;
-    color = envMap->getColor(2246.0, 254.0);
-    std::cout << "2246, 254:" << std::endl;
-    std::cout << std::get<0>(color) << ", ";
-    std::cout << std::get<1>(color) << ", ";
-    std::cout << std::get<2>(color) << std::endl;
-    std::cout << "====================================================" << std::endl;
-
 
     mainPanel.setWorld(&world);
     mainPanel.setCamera(new Scene::Camera());
