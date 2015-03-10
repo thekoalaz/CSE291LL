10f53ab36ea42ccfbca502a5bce2bb578aabd105
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2015-03-09 22:10:08 -0700

    Restored non sorting version.

--- RenderWindow/warp_vert.glsl
+++ RenderWindow/warp_vert.glsl
@@ -49,28 +49,29 @@ varying vec2 uv[3];
 
 ivec3 closestViews(float p0, float p1, float p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9, float p10, float p11)
 {
+    int views[3];
     float prox[12] = {p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11};
-    int index[12] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-
-    for (int cur = 0; cur < 12; cur++)
-    {
-        int cur_max = cur;
-        for (int compare = cur; compare < 12; compare++)
-        {
-            if (prox[cur_max] < prox[compare])
-            {
-                cur_max = compare;
-            }
-        }
-        float prox_temp = prox[cur];
-        prox[cur] = prox[cur_max];
-        prox[cur_max] = prox_temp;
-
-        int index_temp = index[cur];
-        index[cur] = index[cur_max];
-        index[cur_max] = index_temp;
+    float m = 0;
+    for (int i=0; i<12; i++) {
+        if (prox[i]<=m) continue;
+        m = prox[i];
+        views[0] = i;
     }
-    return ivec3(index[0],index[1],index[2]);
+    prox[views[0]] = 0;
+    m = 0;
+    for (int i=0; i<12; i++) {
+        if (prox[i]<=m) continue;
+        m = prox[i];
+        views[1] = i;
+    }
+    prox[views[1]] = 0;
+    m = 0;
+    for (int i=0; i<12; i++) {
+        if (prox[i]<=m) continue;
+        m = prox[i];
+        views[2] = i;
+    }
+    return ivec3(views[0],views[1],views[2]);
 }
 
 void main()
